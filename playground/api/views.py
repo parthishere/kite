246c5a5ba1535a43308db82d791b9735cd490ef5
Manav <manavshah1011.ms@gmail.com>
2023-04-09 17:03:53 +0530

    None

--- playground/api/views.py
+++ playground/api/views.py
@@ -4,12 +4,11 @@ from django.shortcuts import render, redirect
 from kiteconnect import KiteConnect
 from django.conf import settings
 import threading
-from ..views import coreLogic,login_in_zerodha,getPositions, total_pnl
+import logging
+from ..views import coreLogic,login_in_zerodha
 import pyotp
-from ..models import AlgoWatchlist,Instruments, ManualWatchlist, Orders, Preferences
-from .. import models 
-from .serializers import AlgoWatchlistSerializer, PreferencesSerializer, InstrumentsSerializer,OrderSerializer
-from datetime import datetime
+from .. import models
+from . import serializers
 
 kite = KiteConnect(api_key=settings.KITE_API_KEY)
 
@@ -20,56 +19,24 @@ kite = KiteConnect(api_key=settings.KITE_API_KEY)
 
 @api_view(["GET"])
 def login_view(request):    
-    if 'request_token' in request.GET and request.GET.get('request_token'):
-        try:
+        if 'request_token' in request.GET and request.GET.get('request_token'):
             data = kite.generate_session(
                 request.GET['request_token'], api_secret=settings.KITE_API_SECRET)
             kite.set_access_token(data["access_token"])
-        except Exception as e:
-            return Response({"Error": f"Something wrong happed, Error {e}"})
-        coreLogic()
-        return Response({'Data':"Login Successful"})   
-    else:
-        return Response({"Error": "No request token found in callback url"}) 
+            coreLogic()
+            return Response({'Data':"good"})    
 
 @api_view(['GET'])
-def login_with_zerodha(request):            
-    topt = pyotp.TOTP('ZF3MONJ23XF34ESGSGRXOKR6RGTRQLXN')
-    toptKey = topt.now()        
-    kite = login_in_zerodha(settings.KITE_API_KEY, settings.KITE_API_SECRET, 'LN7447', 'zzzzaaaa', toptKey)
-    profile = kite.profile()
-    print(profile)
-    return Response({"Status":"success"})
-
-
-@api_view(["GET"])
-def algowatch(request):
-        
-    if kite.access_token is None:
-        return redirect("/")
-    positionArray = getPositions()
-    totalPNL = total_pnl() or 0
-    algoWatchlistArray = AlgoWatchlist.objects.all()
-    allInstruments = list(Instruments.objects.all(
-    ).values_list('tradingsymbol', flat=True))
-    return Response({'allInstruments': allInstruments, 'algoWatchlistArray': algoWatchlistArray, 'positionArray': positionArray, 'totalPNL': totalPNL})
-
-@api_view(["GET"])
-def manualwatch(request):
-    if kite.access_token is None:
-        return redirect("/")
-    positionArray = getPositions()
-    totalPNL = total_pnl()
-    manualWatchlistArray = ManualWatchlist.objects.all()
-    allInstruments = list(Instruments.objects.all(
-    ).values_list('tradingsymbol', flat=True))
-    return Response({'allInstruments': allInstruments, 'manualWatchlistArray': manualWatchlistArray, 'positionArray': positionArray, 'totalPNL': totalPNL})
-
-
+def algowatch(request):        
+        algowatchlist = models.AlgoWatchlist.objects.all()
+        for obj in algowatchlist:
+               print(obj)
+        return Response("No Response")
+       
 @api_view(['GET'])
 def OrdersApi(reqeust):
-       orders_qs = Orders.objects.all()
-       order_json = OrderSerializer(orders_qs,many=True).data
+       orders_qs = models.Orders.objects.all()
+       order_json = serializers.OrderSerializer(orders_qs,many=True).data
        return Response(order_json)
 
 @api_view(['GET'])
