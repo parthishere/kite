444db87522e97db929e0b254317cb772834e57e7
parthishere <parthishere1234@gmail.com>
2023-04-09 17:29:45 +0530

    parth

--- playground/api/urls.py
+++ playground/api/urls.py
@@ -2,23 +2,19 @@ from . import views
 from django.urls import path,re_path
 
 urlpatterns = [
-<<<<<<< HEAD
     path("login/", views.login_view, name="API-login-view"),
     path("autologin/", views.login_with_zerodha,name="API-Autologin"),
 
     path("algowatch", views.algowatch,name="API-Algowatch"),
     path("manualwatch",  views.manualwatch, name="API-Manualwatch"),
     path("orders",  views.orders, name="API-Orders"),
-    path("settings",  views.settings_view, name="API-Settings"),
-=======
-    path("login/", views.login_view, name="api-login-view"),    
+    path("settings",  views.settings_view, name="API-Settings"), 
     re_path(r'algowatch/$', views.algowatch,name="algowatch"),
     re_path(r'orders/$', views.OrdersApi,name="ordersapi"),
     # path("algowatch",  algowatch,name="Algowatch"),
     # path("manualwatch",  manualwatch, name="Manualwatch"),
     # path("orders",  orders, name="Orders"),
     # path("settings",  api_settings, name="Settings"),
->>>>>>> c3d843bab20bb599fb274f1f6b729e672dad52a1
     # path("logout",  logoutUser, name="Logout"),
 
     # #Ajax function to call from script to view
