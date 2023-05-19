e03e8599a8316b3a1e1439b3c4840faf85f56450
“parthishere” <parthishere1234@gmail.com>
2023-05-19 15:42:23 +0530

    change

--- stop.py
+++ stop.py
@@ -4,35 +4,47 @@ import json
 from time import sleep
 
 url1 = "http://127.0.0.1:8000/api/stopAlgo/"
-# url2 = "http://127.0.0.1:8000/api/startAlgo/"
-
+url2 = "http://127.0.0.1:8000/api/startAlgo/"
 array_instrument = ["NYKAA", "KEC", "UBL", "FEDERALBNK", "SBIN"]
 
-# for i in array_instrument:
-#   sleep(0.2);
-#   payload = json.dumps({
-#   "instrument": i,
-#   "instrumentQuantity": 1,
-#   "is_algo": True
-#   })
-#   headers = {
-#   'Content-Type': 'application/json'
-#   }
+def start(delay):
+    for i in array_instrument:
+        sleep(delay);
+        payload = json.dumps({
+        "instrument": i,
+        "instrumentQuantity": 1,
+        "is_algo": True
+        })
+        headers = {
+        'Content-Type': 'application/json'
+        }
 
-#   response = requests.request("POST", url2, headers=headers, data=payload)
-#   print(response.text)
+        response = requests.request("POST", url2, headers=headers, data=payload)
+        print(response.text)
 
-for i in array_instrument:
-  sleep(0.2);
-  payload = json.dumps({
-    "instrument": i,
-    "instrumentQuantity": 0,
-    "is_algo": True
-  })
-  headers = {
-    'Content-Type': 'application/json'
-  }
+def stop(delay):
+    for i in array_instrument:
+        sleep(delay);
+        payload = json.dumps({
+            "instrument": i,
+            "instrumentQuantity": 0,
+            "is_algo": True
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
 
-  response = requests.request("POST", url1, headers=headers, data=payload)
+        response = requests.request("POST", url1, headers=headers, data=payload)
 
-  print(response.text)
+        print(response.text)
+
+if __name__ == "__main__":
+    while True:
+        a = str(input("enter 's' or 't' to start and stop: ")).lower()
+        b = int(input("enter delay: "))
+        if a == "s":
+            start(b)
+        elif a == "t":
+            stop(b)
+        else:
+            break;
--- test.py
+++ test.py
@@ -1,37 +1,53 @@
+
 import requests
 import json
 from time import sleep
 
 url1 = "http://127.0.0.1:8000/api/stopAlgo/"
 url2 = "http://127.0.0.1:8000/api/startAlgo/"
-
 array_instrument = ["NYKAA", "KEC", "UBL", "FEDERALBNK", "SBIN"]
 
-for i in array_instrument:
-  sleep(0.2);
-  payload = json.dumps({
-  "instrument": i,
-  "instrumentQuantity": 1,
-  "is_algo": True
-  })
-  headers = {
-  'Content-Type': 'application/json'
-  }
+def start(delay):
+    for i in array_instrument:
+        sleep(delay);
+        payload = json.dumps({
+        "instrument": i,
+        "instrumentQuantity": 1,
+        "is_algo": True
+        })
+        headers = {
+        'Content-Type': 'application/json'
+        }
 
-  response = requests.request("POST", url2, headers=headers, data=payload)
-  print(response.text)
+        response = requests.request("POST", url2, headers=headers, data=payload)
+        print(response.text)
 
-# for i in array_instrument:
-#   sleep(0.2);
-#   payload = json.dumps({
-#     "instrument": i,
-#     "instrumentQuantity": 0,
-#     "is_algo": True
-#   })
-#   headers = {
-#     'Content-Type': 'application/json'
-#   }
+def stop(delay):
+    for i in array_instrument:
+        sleep(delay);
+        payload = json.dumps({
+            "instrument": i,
+            "instrumentQuantity": 0,
+            "is_algo": True
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
 
-#   response = requests.request("POST", url1, headers=headers, data=payload)
+        response = requests.request("POST", url1, headers=headers, data=payload)
 
-#   print(response.text)
+        print(response.text)
+
+if __name__ == "__main__":
+    while True:
+        try:
+          a, b = str(input("enter 's' or 't' to start and stop and with space enter delay: (example: 's 0.2') ")).lower().split(" ")
+        except:
+          break;
+        # b = float(input("enter delay: "))
+        if a == "s":
+            start(float(b))
+        elif a == "t":
+            stop(float(b))
+        else:
+            break;
